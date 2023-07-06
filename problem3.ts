335e05340c070140f47ae61539a7679d7756cbd5
rizqiagza <138458784+rizqiagza@users.noreply.github.com>
2023-07-06 10:59:00 +0700

    tugas problem 3

---
+++ problem3.ts
@@ -0,0 +1,6 @@
+// case problem 3
+//output ----- Hello Kobar!Saya Javascript bahasa yag sangat menyenangkan
+let nama : string = "Kobar"
+
+//lgsung identifikasi console
+console.log("Halo" + " " + nama + "!" + "Saya Javascript bahasa yang sangat menyenangkan")
