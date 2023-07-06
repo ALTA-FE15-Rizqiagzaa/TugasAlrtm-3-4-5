8891a801d82d6676456bfe48c331e860d1348114
rizqiagza <138458784+rizqiagza@users.noreply.github.com>
2023-07-06 11:00:03 +0700

    untuk tugas problem 5

---
+++ problem5.ts
@@ -0,0 +1,11 @@
+//case 4 menghitung luas permukaa tabung
+
+//input
+let T : number = 20
+let r : number = 4
+let pi : number = 3.14
+let luas = (2 * pi * r * r + 2 * pi * r * T)
+
+
+console.log(luas)
+
